5e9715b2b30e92e7362cbb8f3c41e78ccbf94aa8
Hari Naik <kumavathharinaik@gmail.com>
2024-06-21 20:58:26 +0530

    Welcome Message

---
+++ src/main/java/org/example/LineComparision.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class LineComparision {
+    public static void main(String[] args) {
+
+        System.out.printf("Welcome to Line Comarision.");
+
+        }
+    }
